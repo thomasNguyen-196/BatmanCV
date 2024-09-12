e085b47049090deb592ad8748480395feda8126a
Tùng Nguyễn Quang <23521743@ms.uit.edu.vn>
2024-09-13 00:12:50 +0700

    thêm vào chức năng màn hình chờ với dòng chữ ở giữa cửa sổ với nội dung là "Click to review detail". Chức năng cơ bản là bấm vào bất cứ đâu trên màn hình chờ thì nội dung của CV người dùng (được làm sẵn, chưa có chức năng cho phép người dùng tự thiết kế và nhập vào).

--- BatmanCV/Form1.Designer.cs
+++ BatmanCV/Form1.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
@@ -94,6 +94,7 @@
             this.label44 = new System.Windows.Forms.Label();
             this.label45 = new System.Windows.Forms.Label();
             this.label46 = new System.Windows.Forms.Label();
+            this.label47 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -119,16 +120,18 @@
             this.panel1.Size = new System.Drawing.Size(731, 150);
             this.panel1.TabIndex = 2;
             // 
-            // pictureBox1
+            // label2
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(208, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(326, 124);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(193)))), ((int)(((byte)(29)))));
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(351, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "#Developer";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
             // 
@@ -143,18 +146,16 @@
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // pictureBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(193)))), ((int)(((byte)(29)))));
-            this.label2.Font = new System.Drawing.Font("Arial Narrow", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(351, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "#Developer";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(208, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label3
             // 
@@ -827,12 +828,24 @@
             this.label46.TabIndex = 64;
             this.label46.Text = "23521525@gm.uit.edu.vn";
             // 
+            // label47
+            // 
+            this.label47.AutoSize = true;
+            this.label47.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label47.Location = new System.Drawing.Point(256, 340);
+            this.label47.Name = "label47";
+            this.label47.Size = new System.Drawing.Size(234, 29);
+            this.label47.TabIndex = 65;
+            this.label47.Text = "Click to review detail";
+            this.label47.Click += new System.EventHandler(this.label47_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
             this.ClientSize = new System.Drawing.Size(729, 875);
+            this.Controls.Add(this.label47);
             this.Controls.Add(this.label46);
             this.Controls.Add(this.label45);
             this.Controls.Add(this.label44);
@@ -897,8 +910,9 @@
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.Color.Gray;
             this.Name = "Form1";
-            this.Text = "v";
+            this.Text = "CV";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -983,6 +997,7 @@
         private System.Windows.Forms.Label label44;
         private System.Windows.Forms.Label label45;
         private System.Windows.Forms.Label label46;
+        private System.Windows.Forms.Label label47;
     }
 }
 
--- BatmanCV/Form1.cs
+++ BatmanCV/Form1.cs
@@ -126,6 +126,73 @@ namespace BatmanCV
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            panel1.Visible = false;
+            pictureBox1.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            pictureBox6.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            linkLabel1.Visible = false;
+            linkLabel2.Visible = false;
+            linkLabel3.Visible = false;
+            linkLabel4.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox7.Visible = false;
+            pictureBox8.Visible = false;
+            pictureBox9.Visible = false;
+            pictureBox10.Visible = false;
+            label11.Visible = false;
+            label12.Visible = false;
+            label13.Visible = false;
+            label14.Visible = false;
+            label15.Visible = false;
+            label16.Visible = false;
+            label17.Visible = false;
+            label18.Visible = false;
+            label19.Visible = false;
+            label20.Visible = false;
+            label21.Visible = false;
+            label22.Visible = false;
+            label23.Visible = false;
+            label24.Visible = false;
+            label25.Visible = false;
+            label26.Visible = false;
+            label27.Visible = false;
+            label28.Visible = false;
+            label29.Visible = false;
+            label30.Visible = false;
+            label31.Visible = false;
+            pictureBox12.Visible = false;
+            label32.Visible = false;
+            label33.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = false;
+            panel4.Visible = false;
+            label34.Visible = false;
+            label35.Visible = false;
+            label36.Visible = false;
+            label37.Visible = false;
+            label38.Visible = false;
+            label39.Visible = false;
+            label40.Visible = false;
+            label41.Visible = false;
+            label42.Visible = false;
+            label43.Visible = false;
+            label44.Visible = false;
+            label45.Visible = false;
+            label46.Visible = false;
+            //label47.Visible = false;
+
 
         }
 
@@ -147,6 +214,146 @@ namespace BatmanCV
         private void label44_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            pictureBox1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            pictureBox6.Visible = true;
+            label8.Visible = true;
+            label9.Visible = true;
+            label10.Visible = true;
+            linkLabel1.Visible = true;
+            linkLabel2.Visible = true;
+            linkLabel3.Visible = true;
+            linkLabel4.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox7.Visible = true;
+            pictureBox8.Visible = true;
+            pictureBox9.Visible = true;
+            pictureBox10.Visible = true;
+            label11.Visible = true;
+            label12.Visible = true;
+            label13.Visible = true;
+            label14.Visible = true;
+            label15.Visible = true;
+            label16.Visible = true;
+            label17.Visible = true;
+            label18.Visible = true;
+            label19.Visible = true;
+            label20.Visible = true;
+            label21.Visible = true;
+            label22.Visible = true;
+            label23.Visible = true;
+            label24.Visible = true;
+            label25.Visible = true;
+            label26.Visible = true;
+            label27.Visible = true;
+            label28.Visible = true;
+            label29.Visible = true;
+            label30.Visible = true;
+            label31.Visible = true;
+            pictureBox12.Visible = true;
+            label32.Visible = true;
+            label33.Visible = true;
+            panel2.Visible = true;
+            panel3.Visible = true;
+            panel4.Visible = true;
+            label34.Visible = true;
+            label35.Visible = true;
+            label36.Visible = true;
+            label37.Visible = true;
+            label38.Visible = true;
+            label39.Visible = true;
+            label40.Visible = true;
+            label41.Visible = true;
+            label42.Visible = true;
+            label43.Visible = true;
+            label44.Visible = true;
+            label45.Visible = true;
+            label46.Visible = true;
+            label47.Visible = false;
+        }
+
+        private void label47_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            pictureBox1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            pictureBox3.Visible = true;
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            pictureBox6.Visible = true;
+            label8.Visible = true;
+            label9.Visible = true;
+            label10.Visible = true;
+            linkLabel1.Visible = true;
+            linkLabel2.Visible = true;
+            linkLabel3.Visible = true;
+            linkLabel4.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox7.Visible = true;
+            pictureBox8.Visible = true;
+            pictureBox9.Visible = true;
+            pictureBox10.Visible = true;
+            label11.Visible = true;
+            label12.Visible = true;
+            label13.Visible = true;
+            label14.Visible = true;
+            label15.Visible = true;
+            label16.Visible = true;
+            label17.Visible = true;
+            label18.Visible = true;
+            label19.Visible = true;
+            label20.Visible = true;
+            label21.Visible = true;
+            label22.Visible = true;
+            label23.Visible = true;
+            label24.Visible = true;
+            label25.Visible = true;
+            label26.Visible = true;
+            label27.Visible = true;
+            label28.Visible = true;
+            label29.Visible = true;
+            label30.Visible = true;
+            label31.Visible = true;
+            pictureBox12.Visible = true;
+            label32.Visible = true;
+            label33.Visible = true;
+            panel2.Visible = true;
+            panel3.Visible = true;
+            panel4.Visible = true;
+            label34.Visible = true;
+            label35.Visible = true;
+            label36.Visible = true;
+            label37.Visible = true;
+            label38.Visible = true;
+            label39.Visible = true;
+            label40.Visible = true;
+            label41.Visible = true;
+            label42.Visible = true;
+            label43.Visible = true;
+            label44.Visible = true;
+            label45.Visible = true;
+            label46.Visible = true;
+            label47.Visible = false;
         }
     }
 }
